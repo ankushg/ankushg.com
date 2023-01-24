54c44fcd2be05edf718435acdbc384a63a5301b4
Ankush Gupta <me@ankushg.com>
2023-01-24 11:20:54 -0800

    auto-import embeds + figure + aside for mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import AutoImport from 'astro-auto-import'
 
 // https://astro.build/config
 import react from '@astrojs/react'
@@ -18,7 +19,13 @@ export default defineConfig({
   site: 'https://ankushg.com',
   markdown: {},
   integrations: [
-    mdx(),
+    AutoImport({
+      imports: [
+        './src/components/Figure.astro',
+        './src/components/Aside.astro',
+        { 'astro-embed': ['Vimeo', 'YouTube', 'Tweet'] },
+      ],
+    }),
     sitemap(),
     react(),
     tailwind(),
@@ -26,6 +33,7 @@ export default defineConfig({
     //   serviceEntryPoint: "@astrojs/image/sharp",
     //   logLevel: "warn",
     // }),
+    mdx(),
   ],
   experimental: {
     contentCollections: true,
